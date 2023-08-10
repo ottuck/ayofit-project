c91076e43f1c230e70a16cf3e315dd9140c46b8c
Sowon <sowon0512@gmail.com>
2023-08-10 18:20:27 +0900

    [Feat] Tab Navigation 기본 틀 작성

---
+++ src/main/ayofront/screens/pedometer/pedometer_test.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function PedometerScreen() {
+  return (
+    <View>
+      <Text>PedometerScreen!</Text>
+    </View>
+  );
+}
+
+export default PedometerScreen;
