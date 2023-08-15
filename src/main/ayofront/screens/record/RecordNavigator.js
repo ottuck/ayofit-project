ebd29435424b4b082b11b140e3d0042bb863dc5c
SangHyun Kim <vison901@gmail.com>
2023-08-16 00:34:12 +0900

    [Degine] 식단등록 RecordPage 뷰 90% 완성
    더 할 부분 :
    1. 컴포넌트화, 모듈화 시켜두기
    2. 슬라이드 라이브러리 적용 여부
    3. 모달 오류 해결

---
+++ src/main/ayofront/screens/record/RecordNavigator.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RecordScreen from "./RecordScreen";
+import RecordMain from "./RecordMain";
+import SearchModal from "./SearchModal";
+
+function RecordNavigator() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="RecordScreen" component={RecordScreen} />
+      <Stack.Screen name="SearchModal" component={SearchModal} options={{presentation: 'modal'}}/>
+      <Stack.Screen name="RecordMain" component={RecordMain} />
+    </Stack.Navigator>
+  );
+}
+export default RecordNavigator
--- src/main/ayofront/screens/record/RecordScreen.js
+++ src/main/ayofront/screens/record/RecordScreen.js
@@ -1,61 +1,169 @@
-import { View, Text, StyleSheet, ImageBackground, Image, SafeAreaView, ScrollView } from "react-native";
-import React from "react";
-import styled from "styled-components/native";
+import { View, Text, StyleSheet, ImageBackground, SafeAreaView, ScrollView, TouchableOpacity, Modal, TextInput } from "react-native";
+import React, { useState } from "react";
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
+function RecordScreen({ navigation }) {
+  const [searchText, setSearchText] = useState("");
+  const searchSubmit = () => {
+    // 여기서 검색어를 처리하거나 필요한 작업을 수행합니다.
+    // 예: 검색 결과를 가져오거나 화면 전환 등
 
-const backgroundImage = require('../../images/background-img.png');
+    // RecordMain.js로 네비게이션을 수행합니다.
+    navigation.navigate("RecordMain");
+  };
 
-// const Nutrient = styled.Text`
-// background-color : white;
-// `;
+  //모달 관련 
+  const [modalVisible, setModalVisible] = useState(false);
+  const openSearchModal = () => {
+    setModalVisible(true);
+  };
+  const closeSearchModal = () => {
+    setModalVisible(false);
+  };
 
-function RecordScreen() {
+  const backgroundImage = require('../../images/background-img.png');
   return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
 
-        <View style={styles.headerContainer}>
-          <Text style={styles.headerTitle}> Diet Record</Text>
-          <Text style={styles.headerDate}> 2023-08-06</Text>
-        </View>
+          <View style={styles.headerContainer}>
+            <Text style={styles.headerTitle}> Diet Record</Text>
+            <Text style={styles.headerDate}> 2023-08-06</Text>
+          </View>
 
-        <ScrollView horizontal={true} contentContainerStyle={styles.cardScroll}>
-          <View style={styles.cardContainer}>
-            <Image source={require('../../images/food-mock-img.png')} style={styles.cardImage} /
-            >
-            <View style={{width:'90%'}}>
-              <View style={styles.textContainer}>
-                <View>
-                  <Text style={styles.mealTime}>BreakFast : </Text>
-                  <Text style={styles.nutrientText}>Carb : </Text>
-                  <Text style={styles.nutrientText}>Protein :  </Text>
-                  <Text style={styles.nutrientText}>Fat : </Text>
-                  <Text style={styles.TotalValue}>Total calories : </Text>
-                </View>
-                <View>
-                  <Text style={styles.mealTime}>AM 09:44</Text>
-                  <Text style={styles.nutrientValue}>55g</Text>
-                  <Text style={styles.nutrientValue}>16.4g</Text>
-                  <Text style={styles.nutrientValue}>21.5g</Text>
-                  <Text style={styles.TotalValue}>487kcal</Text>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.cardScroll}
+          >
+
+
+
+            <View style={styles.cardContainer}>
+              <View style={styles.cardImageContainer}>
+
+                <TouchableOpacity onPress={openSearchModal}>
+                  <Feather name="plus-circle" style={styles.plusIcon} />
+                </TouchableOpacity>
+              </View>
+              <View style={{ width: '90%' }}>
+                <View style={styles.textContainer}>
+                  <View>
+                    <Text style={styles.mealTime}>BreakFast : </Text>
+                    <Text style={styles.nutrientText}>Carb : </Text>
+                    <Text style={styles.nutrientText}>Protein :  </Text>
+                    <Text style={styles.nutrientText}>Fat : </Text>
+                    <Text style={styles.TotalValue}>Total calories : </Text>
+                  </View>
+                  <View>
+                    <Text style={styles.mealTime}>AM 09:44</Text>
+                    <Text style={styles.nutrientValue}>55g</Text>
+                    <Text style={styles.nutrientValue}>16.4g</Text>
+                    <Text style={styles.nutrientValue}>21.5g</Text>
+                    <Text style={styles.TotalValue}>487kcal</Text>
+                  </View>
                 </View>
               </View>
             </View>
-          </View>
-        </ScrollView>
 
-      </ImageBackground>
+            <View style={styles.cardContainer}>
+              <View style={styles.cardImageContainer}>
+
+                <TouchableOpacity onPress={openSearchModal}>
+                  <Feather name="plus-circle" style={styles.plusIcon} />
+                </TouchableOpacity>
+                {/* <TouchableOpacity onPress={() => navigation.navigate("SearchModal")}>
+                  <Feather name="plus-circle" style={styles.plusIcon} />
+                </TouchableOpacity> */}
+              </View>
+              <View style={{ width: '90%' }}>
+                <View style={styles.textContainer}>
+                  <View>
+                    <Text style={styles.mealTime}>BreakFast : </Text>
+                    <Text style={styles.nutrientText}>Carb : </Text>
+                    <Text style={styles.nutrientText}>Protein :  </Text>
+                    <Text style={styles.nutrientText}>Fat : </Text>
+                    <Text style={styles.TotalValue}>Total calories : </Text>
+                  </View>
+                  <View>
+                    <Text style={styles.mealTime}>AM 09:44</Text>
+                    <Text style={styles.nutrientValue}>55g</Text>
+                    <Text style={styles.nutrientValue}>16.4g</Text>
+                    <Text style={styles.nutrientValue}>21.5g</Text>
+                    <Text style={styles.TotalValue}>487kcal</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+
+            <View style={styles.cardContainer}>
+              <View style={styles.cardImageContainer}>
+
+                <TouchableOpacity onPress={openSearchModal}>
+                  <Feather name="plus-circle" style={styles.plusIcon} />
+                </TouchableOpacity>
+              </View>
+              <View style={{ width: '90%' }}>
+                <View style={styles.textContainer}>
+                  <View>
+                    <Text style={styles.mealTime}>BreakFast : </Text>
+                    <Text style={styles.nutrientText}>Carb : </Text>
+                    <Text style={styles.nutrientText}>Protein :  </Text>
+                    <Text style={styles.nutrientText}>Fat : </Text>
+                    <Text style={styles.TotalValue}>Total calories : </Text>
+                  </View>
+                  <View>
+                    <Text style={styles.mealTime}>AM 09:44</Text>
+                    <Text style={styles.nutrientValue}>55g</Text>
+                    <Text style={styles.nutrientValue}>16.4g</Text>
+                    <Text style={styles.nutrientValue}>21.5g</Text>
+                    <Text style={styles.TotalValue}>487kcal</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+
+
+
+          </ScrollView>
+
+          <Modal animationType="slide" visible={modalVisible} transparent={true} >
+            <View style={styles.modalScreen}>
+              <TouchableOpacity onPress={closeSearchModal} >
+                <AntDesign name="close" style={styles.modalCloseButton} />
+              </TouchableOpacity>
+              <View style={styles.modalSearchContainer}>
+                <TouchableOpacity>
+                  <FontAwesome5 name="search" style={styles.modalSearchButton} />
+                </TouchableOpacity>
+                <TextInput
+                  placeholder="Search your food"
+                  style={styles.modalTextInput}
+                  value={searchText}
+                  onChangeText={(text) => setSearchText(text)}
+                  onSubmitEditing={searchSubmit}
+                />
+              </View>
+            </View>
+          </Modal>
+
+        </ImageBackground>
+      </View>
     </SafeAreaView>
   );
 }
 export default RecordScreen;
 
 const styles = StyleSheet.create({
+  safeArea: {
+    // 이미지를 백그라운드 색이랑 합친걸로 교체하면 태그  SafeArea 부분 색을 쉽게 변경 가능
+    backgroundColor: '#E46C0A',
+  },
   container: {
-    flex: 1,
     backgroundColor: '#FFE9D8',
-    // 이미지를 백그라운드 색이랑 합친걸로 교체해야 SafeArea 부분 색을 쉽게 변경 가능
-    // backgroundColor: '#E46C0A',
   },
   backgroundImage: {
     width: '100%',
@@ -66,9 +174,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: '80%',
-    marginTop: 80,
-    marginLeft: 35,   //가운대로 정렬하는 올바른 방법으로 수정 필요함
+    width: 350,
+    marginVertical: 80,
+    marginHorizontal: 35,
   },
   headerTitle: {
     fontWeight: '500',
@@ -79,15 +187,16 @@ const styles = StyleSheet.create({
     color: '#CECECE',
     fontSize: 17,
   },
+  //카드 디자인
   cardScroll: {
     flex: 1,
-    flexDirection: 'row', // 가로 방향 스크롤을 위해 필요한 스타일
-    justifyContent: 'center'
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   cardContainer: {
-    alignSelf: 'center',  //상하 정렬 방법 수정 필요
     width: 300,
-    height: 400,
+    height: 430,
+    marginHorizontal: 20,
     backgroundColor: 'white',
     borderRadius: 15,
     shadowColor: 'black',
@@ -98,31 +207,76 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  cardImage: {
-    height: 270,
+  cardImageContainer: {
     width: 270,
+    height: 270,
+    backgroundColor: 'rgba(0, 0, 0, 0.10)',
+    borderRadius: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  plusIcon: {
+    fontSize: 60,
+    color: 'rgba(0, 0, 0, 0.10)',
   },
   textContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginTop: 20,
   },
   mealTime: {
-    color: 'orange', 
-    fontWeight: 'bold', 
+    color: 'orange',
+    fontWeight: 'bold',
     fontSize: 20,
   },
   nutrientText: {
-    fontSize : 17,
+    fontSize: 17,
     fontWeight: 'bold',
   },
   nutrientValue: {
-    fontSize : 17,
+    fontSize: 17,
     fontWeight: 'bold',
     textAlign: 'right',
   },
   TotalValue: {
-    fontSize : 20,
+    fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'right',
-  }
+  },
+  //모달 디자인
+  modalScreen: {
+    flex: 1,
+    marginTop: '35%',
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    borderRadius: 10,
+  },
+  modalCloseButton: {
+    margin: 10,
+    left: '90%',
+    fontSize: 25,
+    color: '#cecece',
+  },
+  modalSearchContainer: {
+    alignItems: 'center',
+    width: '100%',
+    position: 'relative',
+  },
+  modalTextInput: {
+    width: '80%',
+    height: 45,
+    borderColor: '#cecece',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 50,
+    color: 'orange',
+    fontSize: 16
+  },
+  modalSearchButton: {
+    fontSize: 20,
+    color: 'orange',
+    position: 'absolute',
+    right: '30%',
+    top: 13,
+    zIndex: 1
+  },
 });
