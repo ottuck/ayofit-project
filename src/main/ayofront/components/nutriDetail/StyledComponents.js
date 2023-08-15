b83fe751a2bbb52c08f821a1a09b726af9cd81e3
Jin <songtaejin990@gmail.com>
2023-08-15 20:23:21 +0900

    home 영역 styled components로 작성중, details버튼 눌렸을때와 공통된 top, nav의 보여지는 UI 작성

---
+++ src/main/ayofront/components/nutriDetail/StyledComponents.js
@@ -0,0 +1,116 @@
+import styled from "styled-components/native";
+import * as Progress from "react-native-progress";
+
+export const HomeSafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  background-color: #ffe9d8;
+`;
+
+export const HomeUserContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #ffe9d8;
+  width: 100%;
+  height: 12%;
+`;
+
+export const HomeHelloText = styled.Text`
+  color: #191919;
+  font-weight: 600;
+  font-size: 22px;
+  margin-left: -13px;
+`;
+
+export const HomeAyoText = styled.Text`
+  color: #e46c0a;
+  font-weight: 600;
+  font-size: 20px;
+`;
+
+export const HomeDateText = styled.Text`
+  font-size: 20px;
+  font-weight: 400;
+  margin-left: -13px;
+`;
+
+export const StepProgressContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 16px;
+`;
+
+export const StepProgressBar = styled(Progress.Bar).attrs({
+  color: "#E46C0A",
+  backgroundColor: "rgba(0, 0, 0, 0.45)",
+  borderWidth: 0,
+})``;
+
+export const StepProgressText = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 83.6%;
+  padding-top: 8px;
+`;
+
+export const StepKcalText = styled.Text`
+  color: #000000;
+  font-weight: 400;
+  font-size: 15px;
+`;
+
+export const StepsHighlightText = styled.Text`
+  color: #e46c0a;
+  font-weight: 500;
+  font-size: 15px;
+`;
+
+export const StepsText = styled.Text`
+  color: #000000;
+  font-weight: 400;
+  font-size: 15px;
+`;
+
+export const HomeNavButtonContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: #e46c0a;
+  border-radius: 22px;
+  width: 312px;
+  height: 46px;
+  padding-left: 20px;
+  padding-right: 20px;
+  top: 22px;
+  left: 42px;
+  right: 42px;
+`;
+
+export const HomeNavButton = styled.TouchableOpacity`
+  width: 153px;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => (props.active ? "#FFE1C8" : "transparent")};
+  border-radius: ${(props) => (props.active ? "20px" : "0px")};
+
+  ${(props) =>
+    props.active &&
+    `
+    shadow-color: #000;
+    shadow-offset: 0px 4px;
+    shadow-opacity: 0.0625;
+    shadow-radius: 4px;
+    elevation: 10; 
+  `}
+`;
+
+export const HomeNavButtonText = styled.Text`
+  font-size: 19px;
+  font-weight: 600;
+  letter-spacing: -0.5px;
+  text-align: center;
+  color: ${(props) =>
+    props.active ? "rgba(0, 0, 0, 0.55)" : "rgba(255, 255, 255, 0.55)"};
+`;
--- src/main/ayofront/screens/nutriDetail/nutriDetail_test.js
+++ src/main/ayofront/screens/nutriDetail/nutriDetail_test.js
@@ -1,10 +1,101 @@
-import { View, Text } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  TextInput,
+  SafeAreaView,
+  Animated,
+  Image,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Constants from "expo-constants";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import * as Progress from "react-native-progress";
+import {
+  HomeSafeAreaView,
+  HomeUserContainer,
+  HomeHelloText,
+  HomeDateText,
+  HomeAyoText,
+  StepProgressContainer,
+  StepProgressBar,
+  StepProgressText,
+  StepKcalText,
+  StepsText,
+  StepsHighlightText,
+  HomeNavButtonContainer,
+  HomeNavButton,
+  HomeNavButtonText,
+} from "../../components/nutriDetail/StyledComponents";
 
 function NutriDetailScreen() {
+  const { debuggerHost } = Constants.manifest2.extra.expoGo;
+  const uri = `http://${debuggerHost.split(":").shift()}:8080`;
+
+  const [selectedHomeNavButton, setSelectedHomeNavButton] = useState(0);
+
   return (
-    <View>
-      <Text>NutriDetailScreen!</Text>
-    </View>
+    <HomeSafeAreaView>
+      <StatusBar barStyle="dark-content" />
+      <HomeUserContainer>
+        <View>
+          <HomeHelloText>
+            Hello, <HomeAyoText> Ayo!</HomeAyoText>
+          </HomeHelloText>
+          <HomeDateText>Friday, August 6</HomeDateText>
+        </View>
+        <View>
+          <Image
+            source={require("../../assets/femaleUser.png")}
+            style={{
+              height: 56,
+              width: 56,
+              right: -20,
+            }}
+          />
+        </View>
+      </HomeUserContainer>
+      <StepProgressContainer>
+        <Image
+          source={require("../../assets/personOnBar.png")}
+          style={{
+            height: 38,
+            width: 38,
+            right: -38,
+          }}
+        />
+        <StepProgressBar progress={65 / 100} width={326} height={6.2} />
+        <StepProgressText>
+          <StepKcalText>215 kcal</StepKcalText>
+          <Text>
+            <StepsHighlightText>5,600</StepsHighlightText>
+            <StepsText> / 8000 Steps</StepsText>
+          </Text>
+        </StepProgressText>
+      </StepProgressContainer>
+      <HomeNavButtonContainer>
+        {["My Records", "Details"].map((buttonText, index) => (
+          <HomeNavButton
+            key={buttonText}
+            active={selectedHomeNavButton === index}
+            onPress={() => setSelectedHomeNavButton(index)}
+          >
+            <HomeNavButtonText active={selectedHomeNavButton === index}>
+              {buttonText}
+            </HomeNavButtonText>
+          </HomeNavButton>
+        ))}
+      </HomeNavButtonContainer>
+
+      {/* {selectedHomeNavButton === 0 && <Text>나의 기록</Text>}
+      {selectedHomeNavButton === 1 && <Text>세부 정보</Text>} */}
+      {/* {selectedHomeNavButton === 0 && <MyRecordsComponent />}
+      {selectedHomeNavButton === 1 && <DetailsComponent />} */}
+    </HomeSafeAreaView>
   );
 }
 
