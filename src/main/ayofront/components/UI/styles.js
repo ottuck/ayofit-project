3226ae0cda9f983584b6ff21f9daed8c07fb6bee
Sowon <sowon0512@gmail.com>
2023-08-17 15:32:32 +0900

    [Design] Figma Color Palette RGB Codes

--- src/main/ayofront/components/UI/styles.js
+++ src/main/ayofront/components/UI/styles.js
@@ -1,19 +1,17 @@
 export const GlobalStyles = {
   colors: {
-    primary50: "#F8F0EA",
-    primary100: "#FFE9D8",
-    primary200: "#F3A36F",
-    primary500: "#E46C0A",
-    splashOrange: "#FFB172",
-    gradientYellow: "#FFF3A5",
-    gradientGreen: "#DAE15B",
-    donutChartRed: "#FFD6D1",
-    donutChartYellow: "#FFEC99",
-    donutChartGreen: "#E2F0B5",
+    primary50: "rgb(248,240,234)",
+    primary100: "rgb(255,233,216)",
+    primary200: "rgb(243,163,111)",
+    primary500: "rgb(228,108,10)",
+    splashOrange: "rgb(255,177,114)",
+    gradientYellow: "rgb(255,243,165)",
+    gradientGreen: "rgb(218,225,91)",
+    donutChartRed: "rgb(255,214,209)",
+    donutChartYellow: "rgb(255,236,153)",
+    donutChartGreen: "rgb(226,240,181)",
     whiteOpacity50: "rgba(255,255,255,0.5)",
     blackOpacity10: "rgba(0,0,0,0.1)",
     blackOpacity50: "rgba(0,0,0,0.5)",
   },
 };
-
-
