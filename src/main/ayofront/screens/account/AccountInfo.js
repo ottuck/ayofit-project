f2ae401d4853b337f535c7316272047194e48d41
jiyoon <chupki0531@gmail.com>
2023-08-17 10:50:43 +0900

    [Design] icon버튼 색깔 수정

--- src/main/ayofront/screens/account/AccountInfo.js
+++ src/main/ayofront/screens/account/AccountInfo.js
@@ -4,130 +4,171 @@ import {
   StyleSheet,
   Keyboard,
   TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { GlobalStyles } from "../../components/UI/styles";
-import ImageButton from "../../components/account/UI/ImageButton";
 import Input from "../../components/account/UI/Input";
 import IconButton from "../../components/account/UI/IconButton";
 import Button from "../../components/account/UI/Button";
+import { useState } from "react";
 
 function AccountInfo({ navigation }) {
   const goToAccountNutri = () => {
     navigation.navigate("AccountNutri");
   };
-  const dismissKeyboard = () => {
-    Keyboard.dismiss();
-  };
+
+  const [selectedGender, setSelectedGender] = useState("");
+
+  const [selectedActivity, setSelectedActivity] = useState("");
 
   return (
-    <TouchableWithoutFeedback onPress={dismissKeyboard}>
-      <View style={styles.container}>
-        <View style={styles.top}>
-          <Text style={styles.topText}>Tell us about your goals</Text>
-          <Text style={styles.topText}>to get started.</Text>
-        </View>
-        <View style={styles.genderContainer}>
-          <Text style={styles.text}>Gender</Text>
-          <View style={styles.imageContainer}>
-            <View>
-              <ImageButton
-                label="male"
-                src={require("../../images/male.png")}
-              />
+    <KeyboardAvoidingView
+      style={styles.container}
+      contentContainerStyle={styles.container}
+      behavior={Platform.OS === "android" ? "height" : null}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <View style={styles.top}>
+            <Text style={styles.topText}>Tell us about your goals</Text>
+            <Text style={styles.topText}>to get started.</Text>
+          </View>
+          <View style={styles.genderContainer}>
+            <Text style={styles.text}>Gender</Text>
+            <View style={styles.genderIconContainer}>
+              <View>
+                <IconButton
+                  icon="male"
+                  size={45}
+                  label="male"
+                  onPress={() => setSelectedGender("male")}
+                  color={
+                    selectedGender === "male"
+                      ? GlobalStyles.colors.primary500
+                      : "black"
+                  }
+                  style={
+                    selectedGender === "male" ? styles.genderLabel : "black"
+                  }
+                />
+              </View>
+              <View>
+                <IconButton
+                  icon="female"
+                  size={45}
+                  label="female"
+                  onPress={() => setSelectedGender("female")}
+                  color={
+                    selectedGender === "female"
+                      ? GlobalStyles.colors.primary500
+                      : "black"
+                  }
+                  style={
+                    selectedGender === "female" ? styles.genderLabel : "black"
+                  }
+                />
+              </View>
             </View>
-            <View>
-              <ImageButton
-                label="female"
-                src={require("../../images/female.png")}
+          </View>
+          <View style={styles.ageHeightContainer}>
+            <View style={styles.inputsRow}>
+              <View style={styles.rowInput}>
+                <Input
+                  labelStyle={styles.label}
+                  label="Age"
+                  textInputConfig={{
+                    keyboardType: "decimal-pad",
+                    autoCorrect: false,
+                  }}
+                />
+              </View>
+              <View style={styles.rowInput}>
+                <Input
+                  labelStyle={styles.label}
+                  label="Height"
+                  textInputConfig={{
+                    keyboardType: "decimal-pad",
+                    maxLength: 5,
+                    autoCorrect: false,
+                    placeholder: "cm",
+                  }}
+                />
+              </View>
+            </View>
+          </View>
+          <View style={styles.weightContainer}>
+            <View style={styles.inputsRow}>
+              <View style={styles.rowInput}>
+                <Input
+                  labelStyle={styles.label}
+                  label="Current Weight"
+                  textInputConfig={{
+                    keyboardType: "decimal-pad",
+                    autoCorrect: false,
+                    placeholder: "kg",
+                  }}
+                />
+              </View>
+              <View style={styles.rowInput}>
+                <Input
+                  labelStyle={styles.label}
+                  label="Target Weight"
+                  textInputConfig={{
+                    keyboardType: "decimal-pad",
+                    autoCorrect: false,
+                    placeholder: "kg",
+                  }}
+                />
+              </View>
+            </View>
+          </View>
+          <View style={styles.activityContainer}>
+            <Text style={styles.text}>
+              Give us an idea of your daily activity
+            </Text>
+            <View style={styles.icons}>
+              <IconButton
+                style={styles.high}
+                label="High"
+                icon="slightly-smile"
+                size={40}
+                onPress={() => setSelectedActivity("high")}
+                color={selectedActivity === "high" ? "#CE4257" : "black"}
+              />
+              <IconButton
+                style={styles.moderate}
+                label="Moderate"
+                icon="neutral"
+                size={40}
+                onPress={() => setSelectedActivity("moderate")}
+                color={selectedActivity === "moderate" ? "#FF7F51" : "black"}
+              />
+              <IconButton
+                style={styles.low}
+                label="Low"
+                icon="dizzy"
+                size={40}
+                onPress={() => setSelectedActivity("low")}
+                color={selectedActivity === "low" ? "#FF9B54" : "black"}
               />
             </View>
           </View>
-        </View>
-        <View style={styles.ageHeightContainer}>
-          <View style={styles.inputsRow}>
-            <View style={styles.rowInput}>
-              <Input
-                label="Age"
-                textInputConfig={{
-                  keyboardType: "decimal-pad",
-                  autoCorrect: false,
-                }}
-              />
-            </View>
-            <View style={styles.rowInput}>
-              <Input
-                label="Height"
-                textInputConfig={{
-                  keyboardType: "decimal-pad",
-                  maxLength: 5,
-                  autoCorrect: false,
-                  placeholder: "cm",
-                }}
-              />
-            </View>
+          <View style={styles.next}>
+            <Button style={styles.nextBtn} onPress={goToAccountNutri}>
+              Next
+            </Button>
           </View>
         </View>
-        <View style={styles.weightContainer}>
-          <View style={styles.inputsRow}>
-            <View style={styles.rowInput}>
-              <Input
-                label="Current Weight"
-                textInputConfig={{
-                  keyboardType: "decimal-pad",
-                  autoCorrect: false,
-                  placeholder: "kg",
-                }}
-              />
-            </View>
-            <View style={styles.rowInput}>
-              <Input
-                label="Target Weight"
-                textInputConfig={{
-                  keyboardType: "decimal-pad",
-                  autoCorrect: false,
-                  placeholder: "kg",
-                }}
-              />
-            </View>
-          </View>
-        </View>
-        <View style={styles.activityContainer}>
-          <Text style={styles.text}>
-            Give us an idea of your daily activity
-          </Text>
-          <View style={styles.icons}>
-            <IconButton
-              style={styles.high}
-              label="High"
-              icon="slightly-smile"
-              size={40}
-            />
-            <IconButton
-              style={styles.moderate}
-              label="Moderate"
-              icon="neutral"
-              size={40}
-            />
-            <IconButton style={styles.low} label="Low" icon="dizzy" size={40} />
-          </View>
-        </View>
-        <View style={styles.next}>
-          <Button style={styles.nextBtn} onPress={goToAccountNutri}>
-            Next
-          </Button>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 export default AccountInfo;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: GlobalStyles.colors.primary50,
-  },
+  container: { flex: 1, backgroundColor: GlobalStyles.colors.primary50 },
   top: {
     flex: 1,
     marginTop: 50,
@@ -138,11 +179,11 @@ const styles = StyleSheet.create({
   },
   topText: {
     fontSize: 28,
-    fontWeight: "bold",
+    fontWeight: "700",
   },
   text: {
     fontSize: 20,
-    fontWeight: "400",
+    fontWeight: "600",
     marginBottom: 10,
   },
   genderContainer: {
@@ -150,17 +191,23 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     paddingHorizontal: 20,
   },
-  imageContainer: {
-    flex: 0.8,
+  genderIconContainer: {
+    flex: 0.9,
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
+  },
+  genderLabel: {
+    color: GlobalStyles.colors.primary500,
   },
   ageHeightContainer: {
     flex: 0.7,
     marginHorizontal: 20,
     paddingHorizontal: 20,
     justifyContent: "flex-start",
+  },
+  label: {
+    fontWeight: "600",
   },
   inputsRow: { flexDirection: "row", justifyContent: "space-between" },
   rowInput: {
