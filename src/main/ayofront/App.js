9e291452a526a2a5a5b798c8d8b25293fe947a10
Jin <songtaejin990@gmail.com>
2023-08-18 11:18:34 +0900

    오류 잡고 실행 완료

--- src/main/ayofront/App.js
+++ src/main/ayofront/App.js
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { StyleSheet, Image, Dimensions, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
