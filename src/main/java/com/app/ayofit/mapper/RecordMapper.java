5eaf77751ab51fc86e589ebabf61d4413a8634ff
SangHyun Kim <vison901@gmail.com>
2023-08-18 17:14:49 +0900

    [Feat] Record 기능 스프링 DB 연결
    Controller, Mapper, DTO, DAO 생성

--- src/main/java/com/app/ayofit/mapper/RecordMapper.java
+++ src/main/java/com/app/ayofit/mapper/RecordMapper.java
@@ -1,8 +1,15 @@
 package com.app.ayofit.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.app.ayofit.model.RecordDTO;
 
 @Mapper
 public interface RecordMapper {
-
+	
+	List<RecordDTO> getAllFoodInfo();
+	RecordDTO getFoodInfoById(String n_no);
+	List<RecordDTO> searchFoodByName(String n_food_name);
 }
