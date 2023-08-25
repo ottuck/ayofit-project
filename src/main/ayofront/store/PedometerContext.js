57de182a1c01a719135901c3fa5c1af1d1eb4bf7
Sowon <sowon0512@gmail.com>
2023-08-25 16:42:14 +0900

    [Fix] 하루 걸음 수 저장 로직 구현중

--- src/main/ayofront/store/PedometerContext.js
+++ src/main/ayofront/store/PedometerContext.js
@@ -1,8 +1,14 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useRef,
+} from "react";
 import axios from "axios";
 import Constants from "expo-constants";
 import { Accelerometer } from "expo-sensors";
-import { Animated, Easing } from "react-native";
+import { Animated, Easing, AppState } from "react-native";
 
 const PedometerContext = createContext();
 const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
@@ -61,6 +67,65 @@ export const PedometerProvider = ({ children }) => {
     }, 3000); //
   };
 
+  const handleAppStateChange = async (nextAppState) => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === "active"
+    ) {
+      // 앱이 백그라운드에서 포그라운드로 돌아왔을 때 실행되는 로직
+      // 저장 로직을 여기에 추가
+      await updateOnExit();
+    }
+
+    appState.current = nextAppState;
+  };
+
+  const updateOnExit = async () => {
+    if (steps !== 0) {
+      // 데일리 스텝 카운트가 0이 아니라면 업데이트 수행
+      const userId = "user4"; // 사용자 ID
+      const currentDate = new Date();
+
+      const formattedDate = `${currentDate.getFullYear()}-${(
+        currentDate.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, "0")}-${currentDate
+        .getDate()
+        .toString()
+        .padStart(2, "0")}`;
+
+      // 현재의 steps 값을 서버로 업데이트
+      axios
+        .put(`${uri}/api/pedometer/update-daily-step`, {
+          pId: userId,
+          pDate: formattedDate,
+          pStepCnt: steps, // 현재의 steps 값을 보냅니다.
+        })
+        .then(() => {
+          // 업데이트 성공 시 로그 출력
+          console.log("Daily step count updated successfully.");
+        })
+        .catch((error) => {
+          console.error("Failed to update daily step count:", error);
+        });
+      setSteps(0); // 업데이트 후 스텝 초기화
+    }
+  };
+
+  useEffect(() => {
+    resetStepsOnNewDay();
+
+    AppState.addEventListener("change", handleAppStateChange); // AppState 리스너 추가
+
+    return () => {
+      AppState.removeEventListener("change", handleAppStateChange); // AppState 리스너 제거
+
+      // 앱이 종료될 때 업데이트 로직 수행
+      updateOnExit();
+    };
+  }, []);
+
   //
   const resetStepsOnNewDay = async () => {
     const currentDate = new Date();
@@ -76,50 +141,41 @@ export const PedometerProvider = ({ children }) => {
     }
   };
 
-  const updateDailyStep = () => {
-    const userId = "user4"; // 사용자 ID
-    const currentDate = new Date();
+  // const updateDailyStep = () => {
+  //   const userId = "user4"; // 사용자 ID
+  //   const currentDate = new Date();
 
-    const formattedDate = `${currentDate.getFullYear()}-${(
-      currentDate.getMonth() + 1
-    )
-      .toString()
-      .padStart(2, "0")}-${currentDate.getDate().toString().padStart(2, "0")}`;
+  //   const formattedDate = `${currentDate.getFullYear()}-${(
+  //     currentDate.getMonth() + 1
+  //   )
+  //     .toString()
+  //     .padStart(2, "0")}-${currentDate.getDate().toString().padStart(2, "0")}`;
 
-    // 현재의 steps 값을 서버로 업데이트
-    axios
-      .put(`${uri}/api/pedometer/update-daily-step`, {
-        pId: userId,
-        pDate: formattedDate,
-        pStepCnt: steps, // 현재의 steps 값을 보냅니다.
-      })
-      .then(() => {
-        // 업데이트 성공 시 로그 출력
-        console.log("Daily step count updated successfully.");
-      })
-      .catch((error) => {
-        console.error("Failed to update daily step count:", error);
-      });
-  };
+  //   // 현재의 steps 값을 서버로 업데이트
+  //   axios
+  //     .put(`${uri}/api/pedometer/update-daily-step`, {
+  //       pId: userId,
+  //       pDate: formattedDate,
+  //       pStepCnt: steps, // 현재의 steps 값을 보냅니다.
+  //     })
+  //     .then(() => {
+  //       // 업데이트 성공 시 로그 출력
+  //       console.log("Daily step count updated successfully.");
+  //     })
+  //     .catch((error) => {
+  //       console.error("Failed to update daily step count:", error);
+  //     });
+  // };
 
   // ---------- useEffects ----------
 
   useEffect(() => {
-    loadSavedSteps();
     resetStepsOnNewDay();
   }, []);
 
   useEffect(() => {
-    saveSteps(steps);
     resetStepsOnNewDay();
   }, [steps]);
-
-  useEffect(() => {
-    if (steps !== 0) {
-      // 데일리 스텝 카운트가 0이 아니라면 업데이트 수행
-      updateDailyStep(steps);
-    }
-  }, [steps]); // steps 값이 변할 때마다 업데이트 수행
 
   useEffect(() => {
     const userId = "user4"; // Set the user ID here
@@ -251,7 +307,6 @@ export const PedometerProvider = ({ children }) => {
         stepsToKilometers,
         calculateCaloriesBurned,
         handleGoalUpdate,
-        updateDailyStep,
       }}
     >
       {children}
