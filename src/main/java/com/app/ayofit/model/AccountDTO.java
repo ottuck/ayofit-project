015ff3c3128d856534c2b9fb9e5f3fa0193b8153
arukendev <iamjaehyun4@gmail.com>
2023-08-29 19:27:11 +0900

    횐가입 완료

--- src/main/java/com/app/ayofit/model/AccountDTO.java
+++ src/main/java/com/app/ayofit/model/AccountDTO.java
@@ -1,31 +1,35 @@
 package com.app.ayofit.model;
 
 public class AccountDTO {
+
 	private String l_id;
 	private String l_email;
 	private String l_password;
 	private String l_name;
 	private String l_info;
 	private String l_google;
-	private int a_age;
-	private String a_gender;
-	private double a_height;
-	private double a_weight;
-	private String a_activity;
+	private String gender;
+	private int age;
+	private double height;
+	private double curWeight;
+	private String activity;
+
+	public AccountDTO() {
+	}
 
 	public AccountDTO(String l_id, String l_email, String l_password, String l_name, String l_info, String l_google,
-			int a_age, String a_gender, double a_height, double a_weight, String a_activity) {
+			String gender, int age, double height, double curWeight, String activity) {
 		this.l_id = l_id;
 		this.l_email = l_email;
 		this.l_password = l_password;
 		this.l_name = l_name;
 		this.l_info = l_info;
 		this.l_google = l_google;
-		this.a_age = a_age;
-		this.a_gender = a_gender;
-		this.a_height = a_height;
-		this.a_weight = a_weight;
-		this.a_activity = a_activity;
+		this.gender = gender;
+		this.age = age;
+		this.height = height;
+		this.curWeight = curWeight;
+		this.activity = activity;
 	}
 
 	public String getL_id() {
@@ -76,51 +80,44 @@ public class AccountDTO {
 		this.l_google = l_google;
 	}
 
-	public int getA_age() {
-		return a_age;
+	public String getGender() {
+		return gender;
 	}
 
-	public void setA_age(int a_age) {
-		this.a_age = a_age;
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 
-	public String getA_gender() {
-		return a_gender;
+	public int getAge() {
+		return age;
 	}
 
-	public void setA_gender(String a_gender) {
-		this.a_gender = a_gender;
+	public void setAge(int age) {
+		this.age = age;
 	}
 
-	public double getA_height() {
-		return a_height;
+	public double getHeight() {
+		return height;
 	}
 
-	public void setA_height(double a_height) {
-		this.a_height = a_height;
+	public void setHeight(double height) {
+		this.height = height;
 	}
 
-	public double getA_weight() {
-		return a_weight;
+	public double getCurWeight() {
+		return curWeight;
 	}
 
-	public void setA_weight(double a_weight) {
-		this.a_weight = a_weight;
+	public void setCurWeight(double curWeight) {
+		this.curWeight = curWeight;
 	}
 
-	public String getA_activity() {
-		return a_activity;
+	public String getActivity() {
+		return activity;
 	}
 
-	public void setA_activity(String a_activity) {
-		this.a_activity = a_activity;
-	}
-
-	@Override
-	public String toString() {
-		return "AccountDTO [l_id=" + l_id + ", l_email=" + l_email + ", l_password=" + l_password + ", l_name=" + l_name
-				+ ", l_info=" + l_info + ", l_google=" + l_google + ", a_age=" + a_age + ", a_gender=" + a_gender
-				+ ", a_height=" + a_height + ", a_weight=" + a_weight + ", a_activity=" + a_activity + "]";
+	public void setActivity(String activity) {
+		this.activity = activity;
 	}
 
 }
--- src/main/java/com/app/ayofit/service/LoginDAO.java
+++ src/main/java/com/app/ayofit/service/LoginDAO.java
@@ -2,6 +2,7 @@ package com.app.ayofit.service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -26,7 +27,19 @@ public class LoginDAO {
         if (user.size() != 0) {
             return new LoginDTO("SUCCESS", "Sign in successful", user);
         } else {
-            return new LoginDTO("FAILED", "이메일이나 비번틀림요", null);
+            return new LoginDTO("FAILED", "Email or password is incorrect", null);
+        }
+    }
+
+    public LoginDTO setUser(Map<String, Object> requestData) {
+        String uuid = UUID.randomUUID().toString().split("-")[0];
+        String reqName = (String) requestData.get("name");
+        String reqEmail = (String) requestData.get("email");
+        String reqPassword = (String) requestData.get("password");
+        if (loginMapper.setUser(uuid, reqName, reqEmail, reqPassword) == 1) {
+            return new LoginDTO("SUCCESS", "Sign up successful", null);
+        } else {
+            return new LoginDTO("FAILED", "Sign up failed", null);
         }
     }
 
